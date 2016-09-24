b7ae6b1b2eea08ad134b873357dc6f52feafcc18
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-09-24 23:09:07 +0200

    fixing hdfs import error

--- joblibstore/__init__.py
+++ joblibstore/__init__.py
@@ -1,3 +1,3 @@
-from .backends import register_s3_store_backend
+from .backends import register_s3_store_backend, register_hdfs_store_backend
 
-__all__ = ['register_s3_store_backend']
+__all__ = ['register_s3_store_backend', 'register_hdfs_store_backend']
