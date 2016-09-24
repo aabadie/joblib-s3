3edc46f7dbb2f6a44157c4757347f9b7e95f8df4
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-09-24 21:20:22 +0200

    add example for HDFS (WIP)

---
+++ examples/joblib_hdfs_multiply.py
@@ -0,0 +1,18 @@
+"""Example showing how to use joblib-store to cache results in a HDFS store."""
+
+import numpy as np
+from joblib import Memory
+from joblibstore import register_hdfs_store_backend
+
+if __name__ == '__main__':
+    register_hdfs_store_backend()
+
+    mem = Memory('hdfs://joblib-aa/joblib_cache', host='hdfs.host.com',
+                 port='9999', user='login', verbose=100, compress=True)
+
+    multiply = mem.cache(np.multiply)
+    array1 = np.arange(10000)
+    array2 = np.arange(10000)
+
+    result = multiply(array1, array2)
+    print(result)
