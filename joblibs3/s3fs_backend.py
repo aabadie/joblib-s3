4e3a6f723e0fa8d8670b77d70a1828ef3f1728b6
Rich Fernandez <rich.fernandez@brytercx.com>
2023-02-28 16:46:27 -0700

    fix: clean up bucket and compress variable options.

--- joblibs3/s3fs_backend.py
+++ joblibs3/s3fs_backend.py
@@ -8,14 +8,11 @@ import s3fs
 from joblib._store_backends import StoreBackendBase, StoreBackendMixin
 
 inspect_s3fs = dict(getfullargspec(s3fs.S3FileSystem.__init__)._asdict())
-# Keep compress=False for backwards compatibility
 DEFAULT_BACKEND_OPTIONS = dict(
     zip(
         inspect_s3fs['args'][1:],
         inspect_s3fs['defaults']
-    ),
-    compress=False,
-    bucket=None,
+    )
 )
 del inspect_s3fs
 
@@ -54,15 +51,17 @@ class S3FSStoreBackend(StoreBackendBase, StoreBackendMixin):
     def configure(self, location, verbose=0,
                   backend_options=DEFAULT_BACKEND_OPTIONS):
         """Configure the store backend."""
-        compress = backend_options['compress']
+        compress = backend_options.pop('compress', False)
+        # computation results can be stored compressed for faster I/O
+        self.compress = compress
+
+        bucket = backend_options.pop('bucket', None)
+        if bucket is None:
+            raise ValueError("No valid S3 bucket set")
+
         options = self._check_options(backend_options.copy())
 
         self.storage = s3fs.S3FileSystem(**options)
-
-        if options['bucket'] is None:
-            raise ValueError("No valid S3 bucket set")
-
-        bucket = options['bucket']
 
         # Ensure the given bucket exists.
         root_bucket = os.path.join("s3://", bucket)
@@ -74,9 +73,6 @@ class S3FSStoreBackend(StoreBackendBase, StoreBackendMixin):
         self.location = os.path.join(root_bucket, location)
         if not self.storage.exists(self.location):
             self.storage.mkdir(self.location)
-
-        # computation results can be stored compressed for faster I/O
-        self.compress = compress
 
         # Memory map mode is not supported
         self.mmap_mode = None
